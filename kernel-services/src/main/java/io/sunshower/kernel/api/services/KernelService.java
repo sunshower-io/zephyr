32c65793255ab2fd604cf63ceb3aab32e1f99415
Josiah Haswell <josiah.haswell@gmail.com>
2019-10-08 14:50:05 -0600

    Adding stuff

--- kernel-services/src/main/java/io/sunshower/kernel/api/services/KernelService.java
+++ kernel-services/src/main/java/io/sunshower/kernel/api/services/KernelService.java
@@ -1,12 +1,18 @@
 package io.sunshower.kernel.api.services;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Component;
+
 import javax.annotation.PostConstruct;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("/kernel")
+@Component
 public class KernelService {
 
+    @Requires
+    BundleContext context;
 
 
     @GET
