7ca9c6ba975e82c83a066610037138a6f5226991
Josiah Haswell <josiah.haswell@gmail.com>
2019-11-06 11:09:04 -0700

    Adding support for lock-free scheduling

--- kernel-core/src/test/java/io/sunshower/kernel/concurrency/TopologyAwareParallelSchedulerTest.java
+++ kernel-core/src/test/java/io/sunshower/kernel/concurrency/TopologyAwareParallelSchedulerTest.java
@@ -1,21 +1,30 @@
 package io.sunshower.kernel.concurrency;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 import io.sunshower.gyre.DirectedGraph;
 import io.sunshower.gyre.SerialScheduler;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
 
-import lombok.val;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TopologyAwareParallelSchedulerTest {
 
+  private Context context;
+  private TaskGraph<String> graph;
+
+
   @BeforeEach
-  void setUp() {}
+  void setUp() {
+    graph = new TaskGraph<>();
+    context = new Context();
+  }
 
   @Test
   void ensureSimpleTasksWork() throws ExecutionException, InterruptedException {
@@ -26,30 +35,30 @@ class TopologyAwareParallelSchedulerTest {
         new TopologyAwareParallelScheduler(
             new ExecutorWorkerPool(Executors.newFixedThreadPool(10)));
 
-    for (int i = 0; i < 100; i++) {
-      g.connect(
-          new NamedTask("a") {
-            @Override
-            public Task.TaskValue run(Context c) {
-              results.add(name);
-              return null;
-            }
-          },
-          new NamedTask("b") {
+    g.connect(
+        new NamedTask("a") {
+          @Override
+          public Task.TaskValue run(Context c) {
+            results.add(name);
+            return null;
+          }
+        },
+        new NamedTask("b") {
 
-            @Override
-            public Task.TaskValue run(Context c) {
-              results.add(name);
-              return null;
-            }
-          },
-          DirectedGraph.incoming("a dependsOn b"));
+          @Override
+          public Task.TaskValue run(Context c) {
+            results.add(name);
+            return null;
+          }
+        },
+        DirectedGraph.incoming("a dependsOn b"));
 
-      var topoSchedule = new SerialScheduler<DirectedGraph.Edge<String>, Task>().apply(g);
-      var process = new Process<>(topoSchedule);
-      var result = scheduler.submit(process, new Context());
-
-    }
+    var topoSchedule = new SerialScheduler<DirectedGraph.Edge<String>, Task>().apply(g);
+    var process = new Process<>(topoSchedule);
+    var result = scheduler.submit(process, new Context());
+    result.get();
+    assertEquals(results.get(0), "b");
+    assertEquals(results.get(1), "a");
   }
 
   abstract static class NamedTask implements Task {
