5147b2a22b0db0cc0911d95e116fbbdb0c0eff28
Josiah Haswell <josiah.haswell@gmail.com>
2019-11-21 13:45:05 -0700

    Re-enabling a test

--- kernel-core/src/test/java/io/zephyr/kernel/command/commands/plugin/StartPluginCommandTest.java
+++ kernel-core/src/test/java/io/zephyr/kernel/command/commands/plugin/StartPluginCommandTest.java
@@ -13,7 +13,6 @@ import java.util.NoSuchElementException;
 import java.util.UUID;
 import lombok.val;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 @SuppressWarnings({"PMD.AvoidDuplicateLiterals", "PMD.JUnitAssertionsShouldIncludeMessage"})
@@ -32,7 +31,6 @@ class StartPluginCommandTest extends CommandTestCase {
   }
 
   @Test
-  @Disabled
   void ensureInstallingAndStartingSpringWorksAfterKernelRestart() throws InterruptedException {
     val yamlplugin =
         Tests.relativeToProjectBuild("kernel-modules:sunshower-yaml-reader", "war", "libs");
