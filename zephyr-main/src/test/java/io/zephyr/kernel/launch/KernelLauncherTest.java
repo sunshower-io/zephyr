28c7b0dc1641b98be0bd9d4bcec3eea202374ae7
Josiah Haswell <josiah.haswell@gmail.com>
2020-01-30 20:33:12 -0700

    Adding new entry point

--- zephyr-main/src/test/java/io/zephyr/kernel/launch/KernelLauncherTest.java
+++ zephyr-main/src/test/java/io/zephyr/kernel/launch/KernelLauncherTest.java
@@ -1,60 +1,60 @@
 package io.zephyr.kernel.launch;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 
 import io.zephyr.kernel.extensions.EntryPoint;
 import io.zephyr.kernel.extensions.EntryPointRegistry;
 import lombok.val;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
+@SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
 class KernelLauncherTest {
 
   @Test
   void ensureRegistryIsCreated() {
     val ctx = KernelLauncher.doLaunch(new String[0]);
     val registry = (EntryPointRegistry) ctx.get(EntryPoint.ContextEntries.ENTRY_POINT_REGISTRY);
-    assertNotNull(registry, "registry must not be null");
+    Assertions.assertNotNull(registry, "registry must not be null");
   }
 
   @Test
   void ensureMockEntryPointInitializeIsCalledExactlyOnce() {
     val mep = runAndGetMock();
     val ctx = mep.getContext();
-    verify(mep.getMock(), times(1)).initialize(eq(ctx));
+    Mockito.verify(mep.getMock(), Mockito.times(1)).initialize(eq(ctx));
   }
 
   @Test
   void ensureMockEntryPointStartIsCalledExactlyOnce() {
     val mep = runAndGetMock();
-    verify(mep.getMock(), times(1)).start();
+    Mockito.verify(mep.getMock(), Mockito.times(1)).start();
   }
 
   @Test
   void ensureMockEntryPointRunIsCalledExactlyOnce() {
     val mep = runAndGetMock();
-    verify(mep.getMock(), times(1)).run(mep.getContext());
+    Mockito.verify(mep.getMock(), Mockito.times(1)).run(mep.getContext());
   }
 
   @Test
   void ensureStopIsCalledExactlyOnce() {
     val mep = runAndGetMock();
-    verify(mep.getMock(), times(1)).stop();
+    Mockito.verify(mep.getMock(), Mockito.times(1)).stop();
   }
 
   @Test
   void ensureFinalizeIsCalledExactlyOnce() {
     val mep = runAndGetMock();
-    verify(mep.getMock(), times(1)).finalize(mep.getContext());
+    Mockito.verify(mep.getMock(), Mockito.times(1)).finalize(mep.getContext());
   }
 
   @Test
   void ensureRegistryContainsCorrectEntryPointCount() {
     val ctx = runAndGetMock().getContext();
     val registry = (EntryPointRegistry) ctx.get(EntryPoint.ContextEntries.ENTRY_POINT_REGISTRY);
-    assertEquals(2, registry.getEntryPoints().size(), "must contain 2 entrypoints");
+    Assertions.assertEquals(5, registry.getEntryPoints().size(), "must contain 2 entrypoints");
   }
 
   MockEntryPoint runAndGetMock() {
