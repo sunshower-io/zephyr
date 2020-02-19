250d3f9e0ecc7ddc3a9010672cd127d5a8684d68
Josiah Haswell <josiah.haswell@gmail.com>
2020-02-19 14:32:23 -0700

    Fixing test idempotency issue

--- plugins/barometer/src/test/java/io/zephyr/barometer/BarometerModuleTest.java
+++ plugins/barometer/src/test/java/io/zephyr/barometer/BarometerModuleTest.java
@@ -1,12 +1,15 @@
 package io.zephyr.barometer;
 
+import io.sunshower.kernel.test.Clean;
 import io.sunshower.kernel.test.ZephyrTest;
 import io.zephyr.api.ModuleContext;
 import io.zephyr.kernel.core.Kernel;
 import javax.inject.Inject;
+import lombok.val;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+@Clean
 @ZephyrTest
 public class BarometerModuleTest {
 
@@ -19,10 +22,15 @@ public class BarometerModuleTest {
   @BeforeEach
   void setUp() {
     module = new BarometerModule();
+    val path = kernel.getFileSystem().getPath("barometer/zephyr.pid");
+    if (path.toFile().exists()) {
+      path.toFile().delete();
+    }
   }
 
   @Test
   void ensureStartupIsCorrect() {
+
     module.start(context);
     module.stop(context);
   }
