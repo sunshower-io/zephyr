1977079e5f89bc936ce456e08ba7562659db00b8
Josiah Haswell <josiah.haswell@gmail.com>
2020-08-18 12:28:30 -0600

    Removing mock filesystem

--- kernel-api/src/main/java/io/zephyr/api/ServiceEvents.java
+++ kernel-api/src/main/java/io/zephyr/api/ServiceEvents.java
@@ -1,13 +1,12 @@
 package io.zephyr.api;
 
 import io.zephyr.kernel.events.EventType;
-import lombok.Getter;
 
 public enum ServiceEvents implements EventType {
   REGISTERED,
   UNREGISTERED;
 
-  @Getter private final int id;
+  private final int id;
 
   ServiceEvents() {
     id = EventType.newId();
