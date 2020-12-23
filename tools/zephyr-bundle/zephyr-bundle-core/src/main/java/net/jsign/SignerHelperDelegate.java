9a0b73023fc15d1340ec8f48bb2167964d16a814
Josiah Haswell <josiah.haswell@gmail.com>
2020-12-22 20:06:29 -0700

    Removing jsign for now

--- tools/zephyr-bundle/zephyr-bundle-core/src/main/java/net/jsign/SignerHelperDelegate.java
+++
@@ -1,7 +0,0 @@
-package net.jsign;
-
-public class SignerHelperDelegate extends SignerHelper {
-  public SignerHelperDelegate(Console console, String parameterName) {
-    super(console, parameterName);
-  }
-}
