406cc167297a4f7ae69bbfda2d8480dc5b9ec141
Josiah Haswell <josiah.haswell@gmail.com>
2021-11-09 13:18:34 -0700

    Adding order to dependency

--- kernel-api/src/main/java/io/zephyr/kernel/Dependency.java
+++ kernel-api/src/main/java/io/zephyr/kernel/Dependency.java
@@ -1,11 +1,44 @@
 package io.zephyr.kernel;
 
-import lombok.*;
+import java.util.Comparator;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.val;
 
 @Getter
 @EqualsAndHashCode
-@AllArgsConstructor
-public final class Dependency {
+public final class Dependency implements Comparable<Dependency> {
+
+
+  static final Comparator<Dependency> ORDER_COMPARATOR;
+
+  static {
+    ORDER_COMPARATOR = new OrderComparator();
+  }
+
+  private final int order;
+  private final Type type;
+  private final Coordinate coordinate;
+
+  public Dependency(int order, Type type, Coordinate coordinate) {
+    this.type = type;
+    this.order = order;
+    this.coordinate = coordinate;
+  }
+
+  public Dependency(@NonNull Type type, @NonNull Coordinate coordinate) {
+    this(-1, type, coordinate);
+  }
+
+  public static Comparator<Dependency> orderComparator() {
+    return ORDER_COMPARATOR;
+  }
+
+  @Override
+  public int compareTo(Dependency o) {
+    return coordinate.compareTo(o.coordinate);
+  }
 
   public enum Type {
     Library,
@@ -23,6 +56,11 @@ public final class Dependency {
     }
   }
 
-  final Type type;
-  final Coordinate coordinate;
+  private static final class OrderComparator implements Comparator<Dependency> {
+
+    @Override
+    public int compare(Dependency o1, Dependency o2) {
+      return Integer.compare(o1.order, o2.order);
+    }
+  }
 }
